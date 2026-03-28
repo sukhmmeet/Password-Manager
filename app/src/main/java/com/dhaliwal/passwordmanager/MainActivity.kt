20e8444ce84398d8f0caeb2ea056f394deb782d3
Sukhmeet Singh Dhaliwal <144631710+dhaliwal918@users.noreply.github.com>
2026-03-28 22:14:50 +0530

    Create new project and implement temporary test class and adding dependencies

---
+++ app/src/main/java/com/dhaliwal/passwordmanager/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.dhaliwal.passwordmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.dhaliwal.passwordmanager.ui.theme.PasswordManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PasswordManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    val context = LocalContext.current
+
+                    val test = Test()
+                    test.runFirebase(context)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PasswordManagerTheme {
+        Greeting("Android")
+    }
+}
